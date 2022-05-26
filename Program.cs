f323283ebe79f4bac764b286973287295023a76a
coskunrumeysa <70705198+coskunrumeysa@users.noreply.github.com>
2022-05-26 15:30:56 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+//Beni Yaratan Elbet Yolumu Gösterir -Şuara78
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, surname;
+
+            name = "Rumeysa";
+            surname = "Coskun";
+
+            string town = "Uskudar";
+
+
+            Console.Write(name +" "+ surname+"-"+town);
+            
+
+            Console.Read();
+        }
+    }
+}
